24f1ced711a56280bb5a9d6097ac7d6231b87eed
Depandi Enda <depandienda@gmail.com>
2021-03-18 08:23:32 +0700

    Add welcome message to main activity

--- app/src/main/java/id/ac/polbeng/depandi/testgitapp/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/depandi/testgitapp/MainActivity.kt
@@ -2,10 +2,17 @@ package id.ac.polbeng.depandi.testgitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.polbeng.depandi.testgitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.tvWelcomeMessage.text = getString(R.string.welcome_msg, "Depandi Enda")
     }
 }
